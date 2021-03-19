e09e477646e51a711b1dd0bc7469756f8d0a48a7
Deimos1311 <79268111+Deimos1311@users.noreply.github.com>
2021-03-19 11:33:59 +0300

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -1,5 +1,5 @@
-public class Main {
+public class Task2 {
     public static void main(String[] args) {
-        System.out.println("Java: version 11." + "\nJVM: HotSpot.");
+        System.out.println("Java: version 11.\nJVM: HotSpot.");
     }
 }
